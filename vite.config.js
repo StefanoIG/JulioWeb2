17f1f9c9450bbbdfe539d16dd36ffe40b0fbfed8
StefanoIG <stefano20033@gmail.com>
2024-07-15 08:20:08 -0500

    modify vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,15 +6,15 @@ export default defineConfig({
         outDir: 'dist',
         rollupOptions: {
             input: {
-                admin : resolve(__dirname, 'components/admin.jsx'),
-                crear : resolve(__dirname, 'components/crear.jsx'),
-                editar: resolve(__dirname, 'components/editar.jsx'),
-                estudiante: resolve(__dirname, 'components/estudiante.jsx'),
-                header: resolve(__dirname, 'components/header.jsx'),
-                main: resolve(__dirname, 'components/main.jsx'),
-                MisTutorias: resolve(__dirname, 'components/Mis-Tutorias.jsx'),
-                Tarea: resolve(__dirname, 'components/Tarea.jsx'),
-                Tutorias: resolve(__dirname, 'components/Tutorias.jsx'),
+                admin : resolve(__dirname, './components/admin.jsx'),
+                crear : resolve(__dirname, './components/crear.jsx'),
+                editar: resolve(__dirname, './components/editar.jsx'),
+                estudiante: resolve(__dirname, './components/estudiante.jsx'),
+                header: resolve(__dirname, './components/header.jsx'),
+                main: resolve(__dirname, './components/main.jsx'),
+                MisTutorias: resolve(__dirname, './components/Mis-Tutorias.jsx'),
+                Tarea: resolve(__dirname, './components/Tarea.jsx'),
+                Tutorias: resolve(__dirname, './components/Tutorias.jsx'),
               }
 
         }
