7a97ccc311122ecbb94d11c3ac5ac029fe38cf64
StefanoIG <stefano20033@gmail.com>
2024-07-15 08:30:55 -0500

    refactor: Update file paths in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,12 @@ export default defineConfig({
         outDir: 'dist',
         rollupOptions: {
             input: {
-                admin: resolve(__dirname, 'src/components/admin.jsx'),
-                crear: resolve(__dirname, 'src/components/crear.jsx'),
-                editar: resolve(__dirname, 'src/components/editar.jsx'),
-                estudiante: resolve(__dirname, 'src/components/estudiante.jsx'),
-                header: resolve(__dirname, 'src/components/header.jsx'),
-                main: resolve(__dirname, 'src/components/main.jsx'),
+                admin: resolve(__dirname, 'src/components/Admin.jsx'),
+                crear: resolve(__dirname, 'src/components/Crear.jsx'),
+                editar: resolve(__dirname, 'src/components/Editar.jsx'),
+                estudiante: resolve(__dirname, 'src/components/Estudiante.jsx'),
+                header: resolve(__dirname, 'src/components/Header.jsx'),
+                main: resolve(__dirname, 'src/components/Main.jsx'),
                 MisTutorias: resolve(__dirname, 'src/components/Mis-Tutorias.jsx'),
                 Tarea: resolve(__dirname, 'src/components/Tarea.jsx'),
                 Tutorias: resolve(__dirname, 'src/components/Tutorias.jsx'),
