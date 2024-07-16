ca7941001a3c645c72b4ed01c004a8fcd1e257a7
StefanoIG <stefano20033@gmail.com>
2024-07-16 00:07:27 -0500

    refactor: Update file paths in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,28 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
 export default defineConfig({
-    build: {
-        outDir: 'dist',
-        rollupOptions: {
-            input: {
-                admin: resolve(__dirname, 'src/components/Admin.jsx'),
-                crear: resolve(__dirname, 'src/components/Crear.jsx'),
-                editar: resolve(__dirname, 'src/components/Editar.jsx'),
-                estudiante: resolve(__dirname, 'src/components/Estudiante.jsx'),
-                header: resolve(__dirname, 'src/components/Header.jsx'),
-                main: resolve(__dirname, 'src/components/Main.jsx'),
-                MisTutorias: resolve(__dirname, 'src/components/Mis-Tutorias.jsx'),
-                Tarea: resolve(__dirname, 'src/components/Tarea.jsx'),
-                Tutorias: resolve(__dirname, 'src/components/Tutorias.jsx'),
-            }
-        }
-    },
-    resolve: {
-      alias: {
-          '@': resolve(__dirname, 'src'),
-      },
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        admin: resolve(__dirname, 'src/components/Admin.jsx'),
+        crear: resolve(__dirname, 'src/components/Crear.jsx'),
+        editar: resolve(__dirname, 'src/components/Editar.jsx'),
+        estudiante: resolve(__dirname, 'src/components/Estudiante.jsx'),
+        header: resolve(__dirname, 'src/components/Header.jsx'),
+        main: resolve(__dirname, 'src/components/Main.jsx'),
+        MisTutorias: resolve(__dirname, 'src/components/Mis-Tutorias.jsx'),
+        Tarea: resolve(__dirname, 'src/components/Tarea.jsx'),
+        Tutorias: resolve(__dirname, 'src/components/Tutorias.jsx'),
+      }
+    }
   },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+},
 });
